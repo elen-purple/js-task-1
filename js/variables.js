62067610f9c96039b6bacc152c54fe9beb04e2e1
elen-purple <anna.havrylova.2011@gmail.com>
2024-07-20 15:29:16 +0300

    makes second part of homework

--- js/variables.js
+++ js/variables.js
@@ -11,3 +11,24 @@ const number = myNumber + 10;
 console.log(number);
 const myNull = null;
 console.log(myNull);
+
+const task1 = alert("Перше завдання");
+const yourName = prompt("Please enter your name");
+console.log(yourName);
+
+const task2 = alert("Друге завдання");
+const question = confirm("Are you okay?");
+if (question == true) {
+  const accept = alert("Дякую за підтвердження!");
+} else {
+  const unaccept = alert("Дію відмінено!");
+}
+
+const task3 = alert("Третє завдання");
+const messange = alert("Це небезпечна дія");
+const choice = confirm("Підтвердити виконання дії");
+if (question == true) {
+  const accept = alert("Дякую за підтвердження!");
+} else {
+  const unaccept = alert("Дію відмінено!");
+}
